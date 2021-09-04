e0a6c3aa2ea164b8540bf38789d7a602947b6603
Inka Riyadi <inka.riyadi@gmail.com>
2021-09-04 09:53:29 +0700

    add auto key vigenere encrypt

--- crypto_backend/encryptor/crypto_algorithm/vigenere.py
+++ crypto_backend/encryptor/crypto_algorithm/vigenere.py
@@ -2,7 +2,56 @@
 # A = 65; a = 97
 # Z = 90; z = 122
 
-from .utils import *
+def convert_char_to_base26(character):
+
+    if (character.islower()):
+        return ord(character) - 97
+    
+    else :
+        return ord(character) - 65
+
+    return
+
+# Convert a Base 26 to ASCII
+def convert_base26_to_char(base26):
+    #  Convert to Lowercase
+    ascii_num = base26 + 97
+    return chr(ascii_num)
+
+# Get Inverse of Modulo
+def get_inverse_of_m(m, divider):
+    
+    i = 1
+    while(True):
+        if ((m * i) % divider) == 1:
+            return i 
+        
+        if (i == sys.maxsize):
+            return -1
+
+        i = i + 1
+
+# Is char an alphabet
+def is_alphabet(ch):
+
+    if (ord(ch) >= 65 and ord(ch) <= 90):
+        return True
+
+    if (ord(ch) >= 97 and ord(ch) <= 122):
+        return True
+
+    return False
+
+
+def print_squared_matrix(matrix):
+    matrix_size = len(matrix)
+
+    for i in range(matrix_size):
+        for j in range(matrix_size):
+            if (j == matrix_size - 1):
+                print(matrix[j][i])
+            else :
+                print(matrix[j][i], end=' ')
 
 # Vigenere Cipher Basic
 
@@ -99,3 +148,28 @@ def vigenere_decrypt(cipher, key):
 
 
 # Auto-Key Vigenere Cipher
+def auto_key_encrypt(plain,key):
+    
+    plain = plain.lower()
+    
+    plain_length = len(plain)
+    key_length = len(key)
+
+    cipher = ""
+    
+    #If key is shorter than plain text, concatenate with plaintext
+    if(key_length<plain_length):
+        key_loop = key
+        for i in range(plain_length-key_length):
+            key_loop = key_loop + plain[i]
+
+    #Encryption
+    for i in range(plain_length):
+        p = convert_char_to_base26(plain[i])
+        k = convert_char_to_base26(key_loop[i])
+        c_in_ascii = (p + k) % 26
+        c = convert_base26_to_char(c_in_ascii)
+        cipher = cipher + c
+
+    return cipher
+    
